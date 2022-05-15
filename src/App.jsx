90e03d6a1797f910e27c0d3d66044b28888d4780
Matias Arias <matt.arias182@gmail.com>
2022-05-15 07:38:08 -0300

    añadiendo react router para la navegación, y google login

--- src/App.jsx
+++ src/App.jsx
@@ -1,103 +1,21 @@
-import React, { useEffect, useState } from 'react'
-import { getCoins } from './helpers/coinFetch'
-import numeral from 'numeral'
-import CoinIcon from './components/CoinIcon';
-import LoadingIcon from './components/LoadingIcon';
+import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Error404 from './pages/Error404';
+import Home from './pages/Home';
+import LoginScreen from './pages/LoginScreen';
 
-import './css/styles.css'
-import CoinNav from './components/CoinNav';
-
+import "./css/styles.css";
 
 const App = () => {
-
-    const [coins, setCoins] = useState({ loading: true, datos: [] })
-
-    const [update, setUpdate] = useState(false)
-
-    useEffect(() => {
-
-        getCoins().then((respuesta) => {
-
-            console.log(respuesta);
-
-            setCoins({
-                loading: false,
-                datos: respuesta
-            })
-
-        });
-    }, [update]);
-
     return (
-
-        <>
-            <CoinNav />
-
-            <div className='container'>
-                <div className="row">
-                    <div className="col">
-                        {/* <h1>Coin App</h1> */}
-                    </div>
-                </div>
-
-                <div className="row mt-5">
-                    <div className="col">
-
-                        {coins.loading ? <LoadingIcon /> : (
-
-                            <table className='table table-coin'>
-                                <thead>
-                                    <tr>
-                                        <th>Rank</th>
-                                        <th>Name</th>
-                                        <th>Price Usd</th>
-                                        <th>Market Cap</th>
-                                        <th>VWAP (24hs)</th>
-                                        <th>Supply</th>
-                                        <th>Volumne (24hr)</th>
-                                        <th>Change (24hr)</th>
-                                        <th> <button className='btn-coin' onClick={() => setUpdate(!update)}>Update</button></th>
-                                    </tr>
-                                </thead>
-
-                                <tbody>
-
-                                    {
-                                        coins.datos.map((coin) => (
-
-                                            <tr key={coin.id}>
-                                                <td className='text-center'>{coin.rank}</td>
-
-                                                <td className='d-flex justify-content-between'>
-                                                    <CoinIcon symbol={coin.symbol} />
-                                                    {coin.name}
-                                                </td>
-
-                                                <td>{numeral(coin.priceUsd).format('$0,0.00')}</td>
-                                                <td>{numeral(coin.marketCapUsd).format('($ 0.00 a)')}</td>
-                                                <td>{numeral(coin.vwap24Hr).format('$0,0.00')}</td>
-                                                <td>{numeral(coin.supply).format('($ 0.00 a)')}</td>
-                                                <td>{numeral(coin.volumeUsd24Hr).format('($ 0.00 a)')}</td>
-                                                <td className={coin.changePercent24Hr > 0 ? 'text-success' : 'text-danger'}>{parseFloat(coin.changePercent24Hr).toFixed(2)}%</td>
-                                            </tr>
-                                        ))
-                                    }
-
-                                </tbody>
-                            </table>
-
-                        )}
-
-                    </div>
-                </div>
-            </div>
-
-        </>
-
-
-
-
-    )
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="login" element={<LoginScreen />} />
+                <Route path="*" element={<Error404 />} />
+            </Routes>
+        </BrowserRouter>
+    );
 };
 
-export default App
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,99 @@
+import React, { useEffect, useState } from "react";
+import { getCoins } from "../helpers/coinFetch";
+import numeral from "numeral";
+import CoinIcon from "../components/CoinIcon";
+import LoadingIcon from "../components/LoadingIcon";
+// import CoinNav from "../CoinNav/components/CoinNav";
+
+const Home = () => {
+  const [coins, setCoins] = useState({ loading: true, datos: [] });
+
+  const [update, setUpdate] = useState(false);
+
+  useEffect(() => {
+    getCoins().then((respuesta) => {
+      console.log(respuesta);
+
+      setCoins({
+        loading: false,
+        datos: respuesta,
+      });
+    });
+  }, [update]);
+
+  return (
+    <>
+
+      <div className="container">
+        <div className="row">
+          <div className="col">{/* <h1>Coin App</h1> */}</div>
+        </div>
+
+        <div className="row mt-5">
+          <div className="col">
+            {coins.loading ? (
+              <LoadingIcon />
+            ) : (
+              <table className="table table-coin">
+                <thead>
+                  <tr>
+                    <th>Rank</th>
+                    <th>Name</th>
+                    <th>Price Usd</th>
+                    <th>Market Cap</th>
+                    <th>VWAP (24hs)</th>
+                    <th>Supply</th>
+                    <th>Volumne (24hr)</th>
+                    <th>Change (24hr)</th>
+                    <th>
+                      {" "}
+                      <button
+                        className="btn-coin"
+                        onClick={() => setUpdate(!update)}
+                      >
+                        Update
+                      </button>
+                    </th>
+                  </tr>
+                </thead>
+
+                <tbody>
+                  {coins.datos.map((coin) => (
+                    <tr key={coin.id}>
+                      <td className="text-center">{coin.rank}</td>
+
+                      <td className="d-flex justify-content-between">
+                        <CoinIcon symbol={coin.symbol} />
+                        {coin.name}
+                      </td>
+
+                      <td>{numeral(coin.priceUsd).format("$0,0.00")}</td>
+                      <td>{numeral(coin.marketCapUsd).format("($ 0.00 a)")}</td>
+                      <td>{numeral(coin.vwap24Hr).format("$0,0.00")}</td>
+                      <td>{numeral(coin.supply).format("($ 0.00 a)")}</td>
+                      <td>
+                        {numeral(coin.volumeUsd24Hr).format("($ 0.00 a)")}
+                      </td>
+                      <td
+                        className={
+                          coin.changePercent24Hr > 0
+                            ? "text-success"
+                            : "text-danger"
+                        }
+                      >
+                        {parseFloat(coin.changePercent24Hr).toFixed(2)}%
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            )}
+          </div>
+        </div>
+      </div>
+
+    </>
+  );
+};
+
+export default Home;
